6204e671124433272e51b11eaf98c60b71688cfa
zuza-wjc <wojcik.zuza@yahoo.com>
2024-12-01 22:42:24 +0100

    ramki zmieniają kolor

--- DeckbuilderWybory/Assets/Scripts/GameScene/PlayerStats/StatsCard.cs
+++ DeckbuilderWybory/Assets/Scripts/GameScene/PlayerStats/StatsCard.cs
@@ -13,6 +13,14 @@ public class StatsCard : MonoBehaviour
     public Text playerTurnNumberText;
     public Text deckTypeText;
 
+    public Image deckTypeImage;
+
+    public Sprite ambasadaSprite;
+    public Sprite metropoliaSprite;
+    public Sprite srodowiskoSprite;
+    public Sprite przemyslSprite;
+    public Sprite podstawaSprite;
+
     public void SetPlayerData(string playerName, string playerSupport, string playerMoney, string playerIncome, int playerCardNumber, string regionSupport, int turnNumber, string deckType)
     {
         playerNameText.text = playerName;
@@ -23,5 +31,36 @@ public class StatsCard : MonoBehaviour
         playerRegionSupportText.text = regionSupport;
         playerTurnNumberText.text = turnNumber.ToString();
         deckTypeText.text = deckType;
+
+        SetDeckTypeImage(deckType);
+    }
+
+    private void SetDeckTypeImage(string deckType)
+    {
+        if (deckTypeImage == null)
+        {
+            Debug.LogError("DeckTypeImage is not assigned!");
+            return;
+        }
+
+        switch (deckType.ToLower()) // Przekszta³camy na ma³e litery dla bezb³êdnego porównania
+        {
+            case "ambasada":
+                deckTypeImage.sprite = ambasadaSprite;
+                break;
+            case "metropolia":
+                deckTypeImage.sprite = metropoliaSprite;
+                break;
+            case "œrodowisko":
+                deckTypeImage.sprite = srodowiskoSprite;
+                break;
+            case "przemys³":
+                deckTypeImage.sprite = przemyslSprite;
+                break;
+            default:
+                Debug.LogWarning($"Unknown deckType: {deckType}. Default sprite will be used.");
+                deckTypeImage.sprite = podstawaSprite;
+                break;
+        }
     }
 }
