4b1c6a494dbe527538d8d880d47b563a7d7304b6
zuzmys3 <z.myszczuk@yahoo.pl>
2024-12-18 12:44:39 +0100

    zmiana ui,plus minus guzik

--- DeckbuilderWybory/Assets/Scripts/DeckCreationScene/AddCardsPanelController.cs
+++ DeckbuilderWybory/Assets/Scripts/DeckCreationScene/AddCardsPanelController.cs
@@ -1,18 +1,101 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase.Database;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using Firebase;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+using System.Linq;
+using System;
 
 public class AddCardsPanelController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text lobbySizeText;
+    public Button plusButton;
+    public Button minusButton;
+
+    private Image addButtonImage;
+    private Image minusButtonImage;
+    public Sprite addButtonActiveSprite;
+    public Sprite minusButtonActiveSprite;
+    public Sprite minusButtonInactiveSprite;
+    public Sprite addButtonInactiveSprite;
+
+    int cardsCount = 0;
+
     void Start()
     {
-        
+        addButtonImage = plusButton.GetComponentInChildren<Image>();
+        minusButtonImage = minusButton.GetComponentInChildren<Image>();
+
+        plusButton.onClick.AddListener(IncreaseLobbySize);
+        minusButton.onClick.AddListener(DecreaseLobbySize);
+
+        UpdateLobbySizeText();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void IncreaseLobbySize()
     {
-        
+        if (cardsCount < 2)
+        {
+            cardsCount++;
+            UpdateLobbySizeText();
+        }
     }
+
+    public void DecreaseLobbySize()
+    {
+        if (cardsCount > 0)
+        {
+            cardsCount--;
+            UpdateLobbySizeText();
+        }
+    }
+
+    void UpdateLobbySizeText()
+    {
+        lobbySizeText.text = cardsCount.ToString();
+
+        plusButton.interactable = cardsCount < 2;
+        minusButton.interactable = cardsCount > 0;
+
+        UpdateButtonSprites();
+    }
+
+    void UpdateButtonSprites()
+    {
+        if (plusButton.interactable)
+        {
+            addButtonImage.sprite = addButtonActiveSprite;
+        }
+        else
+        {
+            addButtonImage.sprite = addButtonInactiveSprite;
+        }
+
+        if (minusButton.interactable)
+        {
+            minusButtonImage.sprite = minusButtonActiveSprite;
+        }
+        else
+        {
+            minusButtonImage.sprite = minusButtonInactiveSprite;
+        }
+    }
+
+    void OnDestroy()
+    {
+
+        if (plusButton != null)
+        {
+            plusButton.onClick.AddListener(IncreaseLobbySize);
+        }
+
+        if (minusButton != null)
+        {
+            minusButton.onClick.AddListener(DecreaseLobbySize);
+        }
+    }
+
 }
